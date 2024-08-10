4a793fca4802080d270637076c0810924b0d7cce
Syed Zaid Ali <zaidali753@gmail.com>
2024-08-10 22:39:29 +0530

    Removed unused main file

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.api.endpoints.router import router as calculation_router
-
-
-app = FastAPI()
-
-app.include_router(calculation_router, prefix="/splits", tags=["splits"])
-
